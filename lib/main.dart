8dad2aa701fb3c976dbe15d2daab76630575418c
Leo18ss <lsstdm@live.com>
2021-07-09 14:41:21 -0300

    Adicionando leitura e escrita dos dados

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,13 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
+import 'dart:io';
+import 'package:path_provider/path_provider.dart';
 
 void main() {
   runApp(MaterialApp(
     home: Home(),
     theme: ThemeData(hintColor: Colors.amber, primaryColor: Colors.white),
+    debugShowCheckedModeBanner: false,
   ));
 }
 
@@ -18,11 +22,39 @@ class _HomeState extends State<Home> {
   final novoSalController = TextEditingController();
 
   double doubleSalario, doubleAumento;
+  String salarioAtual;
+
+  @override
+  void initState() {
+    read();
+    super.initState();
+  }
+
+  write(String text) async {
+    final Directory directory = await getApplicationDocumentsDirectory();
+    final File file = File('${directory.path}/valores.txt');
+    await file.writeAsString(text);
+  }
+
+  Future<void> read() async {
+    try {
+      final Directory directory = await getApplicationDocumentsDirectory();
+      final File file = File('${directory.path}/valores.txt');
+      salarioAtual = await file.readAsString();
+    } catch (e) {
+      print("Nao foi possivel ler o arquivo.");
+    }
+  }
 
   void _clearAll() {
     salarioController.text = "";
     aumentoController.text = "";
     novoSalController.text = "";
+  }
+
+  _salarioAtual() async {
+    await read();
+    novoSalController.text = salarioAtual;
   }
 
   _aumentoNormal() {
@@ -34,6 +66,7 @@ class _HomeState extends State<Home> {
     doubleAumento = double.parse(aumentoController.text);
     novoSalController.text =
         ((doubleSalario + doubleAumento).toStringAsFixed(2));
+    write(novoSalController.text);
   }
 
   _aumentoPorcentagem() {
@@ -46,6 +79,7 @@ class _HomeState extends State<Home> {
     novoSalController.text =
         (doubleSalario + (doubleSalario * doubleAumento / 100))
             .toStringAsFixed(2);
+    write(novoSalController.text);
   }
 
   @override
@@ -66,7 +100,7 @@ class _HomeState extends State<Home> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.stretch,
             children: <Widget>[
-              Icon(Icons.attach_money, size: 120.0, color: Colors.amber),
+              Icon(Icons.attach_money, size: 100.0, color: Colors.amber),
               SizedBox(
                 height: 8,
               ),
@@ -126,6 +160,25 @@ class _HomeState extends State<Home> {
                   child: ElevatedButton(
                     style: ElevatedButton.styleFrom(primary: Colors.amber),
                     onPressed: () {
+                      _salarioAtual();
+                    },
+                    child: Text(
+                      "Salario atual",
+                      style: TextStyle(color: Colors.black, fontSize: 16),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              Center(
+                child: SizedBox(
+                  height: 40,
+                  width: 150,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(primary: Colors.amber),
+                    onPressed: () {
                       _clearAll();
                     },
                     child: Text(
@@ -134,7 +187,7 @@ class _HomeState extends State<Home> {
                     ),
                   ),
                 ),
-              )
+              ),
             ],
           ),
         ),
